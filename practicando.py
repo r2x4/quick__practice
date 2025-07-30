8423aa9f274404abab9311603123beb19f9d7fd9
Rodrigo <rrs230780@gmail.com>
2025-07-30 17:12:25 -0500

    guardar_12

---
+++ practicando.py
@@ -0,0 +1,18 @@
+# estructuras de control
+
+edad = 15
+
+if edad < 18:
+    print("Es menor de edad")
+
+elif edad >= 18 and edad < 60:
+    print("Eres un Adulto")
+
+elif edad > 15:  # esta afecta la secuencia
+    print("Mayor de 15 Anos ")
+
+elif edad == 60:
+    print("Feliz cumple 60")
+
+else:
+    print("Eres un adulto Mayor")
